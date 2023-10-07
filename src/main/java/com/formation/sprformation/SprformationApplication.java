8baa54d3cd4a8a0fd45674a1ec35dd6f0b1ad3d5
AdnenKhiari <aymenkhiari484@gmail.com>
2023-10-07 12:19:14 +0100

    init

---
+++ src/main/java/com/formation/sprformation/SprformationApplication.java
@@ -0,0 +1,13 @@
+package com.formation.sprformation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprformationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprformationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/formation/sprformation/SprformationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formation.sprformation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprformationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
